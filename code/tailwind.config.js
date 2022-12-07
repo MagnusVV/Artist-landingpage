134d31cc77a27c874b034a23a0a18353c8cf641d
Styrbjörn Nordberg <styris.n@gmail.com>
2022-12-07 16:31:46 +0100

    built mobile.
    Co-authored-by: Magnus Vargvinter

--- code/tailwind.config.js
+++ code/tailwind.config.js
@@ -5,13 +5,17 @@ module.exports = {
     "./src/**/*"
 ],
   theme: {
+    fontFamily: {
+      'sans':['"noto-sans"', 'sans-serif']
+    },
     extend: {
       colors: {
         "primary":"#9F824A",
         "secundary":"#45292A",
         "tertiary":"#090809",
+        "link-color":"#234A57",
       }
-    },
+    }, 
   },
   plugins: [],
 }
