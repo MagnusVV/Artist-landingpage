bf764dc92060ee108ba0949fa5a991105f85c44a
MagnusVV <svartkub@msn.com>
2022-12-14 15:10:43 +0100

    Downloade Noto-sans

--- code/tailwind.config.js
+++ code/tailwind.config.js
@@ -1,21 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html, js, ts, vue}", 
-    "./src/**/*"
-],
+  content: ["./src/**/*.{html, js, ts, vue}", "./src/**/*"],
   theme: {
-    fontFamily: {
-      'sans':['"noto-sans"', 'sans-serif']
-    },
     extend: {
       colors: {
-        "primary":"#9F824A",
-        "secundary":"#45292A",
-        "tertiary":"#090809",
-        "link-color":"#234A57",
-      }
-    }, 
+        primary: "#9F824A",
+        secundary: "#45292A",
+        tertiary: "#090809",
+        "link-color": "#234A57",
+      },
+    },
   },
   plugins: [],
-}
+};
